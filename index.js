3d0196e1a38e6ea969060097d74f8fd8e4a6646e
Njalale <fnjalale@gmail.com>
2023-03-13 20:04:24 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber'
+
+function companyName(){}
+     /const companyName/
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+function mostProfitableNeighborhood(){}
+     /let mostProfitableNeighborhood/
+
+let companyCeo = 'Susan Smith'
+
+function companyCeo(){}
+     /let companyCeo/
